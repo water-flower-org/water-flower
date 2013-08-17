e2932d652efa432a98993b212bfa80e3cc5f500a
xsara <hangar18@163.com>
2013-08-17 23:46:55 +0800

    added new view

---
+++ water-flower/ss_trialThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  ss_trialThirdViewController.m
+//  water-flower
+//
+//  Created by ss on 8/17/13.
+//  Copyright (c) 2013 ss. All rights reserved.
+//
+
+#import "ss_trialThirdViewController.h"
+
+@interface ss_trialThirdViewController ()
+
+@end
+
+@implementation ss_trialThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
